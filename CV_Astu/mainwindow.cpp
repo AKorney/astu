a30cfb710dec2061166b22a3b9d063291bf34e10
AKorney <korney.alena@yandex.ru>
2017-03-20 13:39:56 +0700

    real test

--- CV_Astu/mainwindow.cpp
+++ CV_Astu/mainwindow.cpp
@@ -29,21 +29,28 @@ MainWindow::~MainWindow()
 void MainWindow::on_openButton_clicked()
 {
     
-    const auto sourceImage = ImageHelper::Load(ui->lineEdit->text());
+    const auto sourceImage1 = ImageHelper::Load("C:\\Users\\Alena\\Pictures\\descr\\test1.png");
+    const auto sourceImage2 = ImageHelper::Load("C:\\Users\\Alena\\Pictures\\descr\\test2.png");
     auto detector = InterestingPointsDetector(DetectionMethod::Harris);
-    //auto diffs = detector.CalculateDiffs(3, BorderType::Replicate);
-    //CVImageLoader::Save("C:\\Users\\Alena\\Pictures\\Diffs.jpg", CVImage(diffs));
-    auto points = detector.FindInterestingPoints(sourceImage.PrepareDoubleMat()
+
+
+    auto points1 = detector.FindInterestingPoints(sourceImage1.PrepareDoubleMat()
                                                  .Convolve(KernelBuilder::BuildGauss(1),
                                                            BorderType::Replicate),
                                                  3, 0.075);
-    auto qImage = ImageHelper::MarkInterestingPoints(sourceImage, points);
-    qImage.save("C:\\Users\\Alena\\Pictures\\pointsHarris.jpg");
+    auto points2 = detector.FindInterestingPoints(sourceImage2.PrepareDoubleMat()
+                                                 .Convolve(KernelBuilder::BuildGauss(1),
+                                                           BorderType::Replicate),
+                                                 3, 0.075);
+
     auto descriptorBuilder = DescriptorsBuilder();
-    auto descriptors = descriptorBuilder.CalculateSimpleDescriptors(sourceImage.PrepareDoubleMat(), points);
-    auto matches = DescriptorsBuilder::FindMatches(descriptors, descriptors);
-    auto extended = ImageHelper::DrawMatches(sourceImage, sourceImage, matches);
-    extended.save("C:\\Users\\Alena\\Pictures\\matches.jpg");
+    auto descriptors1 = descriptorBuilder.CalculateSimpleDescriptors
+            (sourceImage1.PrepareDoubleMat(), points1);
+    auto descriptors2 = descriptorBuilder.CalculateSimpleDescriptors
+            (sourceImage2.PrepareDoubleMat(), points2);
+    auto matches = DescriptorsBuilder::FindMatches(descriptors1, descriptors2);
+    auto extended = ImageHelper::DrawMatches(sourceImage1, sourceImage2, matches);
+    extended.save("C:\\Users\\Alena\\Pictures\\descr\\matches.jpg");
 }
 
 
